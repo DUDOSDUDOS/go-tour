4e78fbf4fbdb0d5431183cf1c057b6bb338cdf46
DUDOSDUDOS <61326683+DUDOSDUDOS@users.noreply.github.com>
2020-02-23 00:24:26 +0200

    Выполненное задание

--- main.go
+++ main.go
@@ -1,6 +1,37 @@
 package main
 
-func main () {
-  println("HELLO")
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", mainPage)
+
+	port := ":9999"
+	println("Conected to port:", port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatal("FATAL ERROR")
+	}
+
+}
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+	//user := User{"Кирилл", "Яйцеглист"}
+	//js, _ := json.Marshal(user)
+
+	tmpl, err := template.ParseFiles("static/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
 
 }
